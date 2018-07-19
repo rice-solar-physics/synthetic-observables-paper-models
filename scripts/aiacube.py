4eb30653d5ae398acffa69e823bdae4b9be63a1e
Will Barnes <wtb2@rice.edu>
2018-07-19 12:39:54 -0500

    refactor timelag scripts to make better use of dask+fits, no more building HDF5 files

--- scripts/aiacube.py
+++ scripts/aiacube.py
@@ -1,13 +1,13 @@
 """
 Object for dealing efficiently with large out-of-core AIA datacubes.
-Much of this was inspired by work done by Stuart Mumford, in particular how to handle
+Nearly all of this is inspired by work done by Stuart Mumford, in particular how to handle
 FITS files in Dask.
 """
 import dask.bytes
 import dask.array as da
 import dask
 import distributed
-from dask import delayed,compute
+from dask import delayed, compute
 from astropy.io import fits
 from astropy.time import Time
 import astropy.units as u
@@ -46,58 +46,93 @@ class DelayedFITS:
 
 class DistributedAIACube(object):
     """
-    Load sequences of AIA images over multiple channels and operate on them
-    in a distributed and parallelized way.
+    Load sequence of AIA images for a single channel and operate on them in a distributed and 
+    parallelized way.
+
+    #TODO: Refactor this to use ndcube instead
     """
-    def __init__(self, read_template, channels=None):
+    def __init__(self, maps, headers):
+        self.maps = maps
+        self.headers = headers
+
+    @classmethod
+    def from_files(cls, read_template):
+        openfiles = dask.bytes.open_files(read_template)
+        headers = cls._get_headers(openfiles)
+        dtype, shape = cls._get_dtype_and_shape(headers)
+        maps = cls._get_maps(openfiles, headers, dtype, shape)
+        return cls(maps, headers, dtype, shape)
+
+    @staticmethod
+    def _get_maps(self, openfiles, headers, dtype, shape):
+        arrays = [da.from_array(DelayedFITS(f, shape=shape, dtype=dtype, hdu=0),
+                                chunks=shape) for f in openfiles]
+        return [sunpy.map.Map(a, h) for a, h in zip(arrays, headers)]
+
+    @staticmethod
+    def _get_headers(openfiles):
         client = distributed.get_client()
-        self.channels = ['94','131','171','193','211','335'] if channels is None else channels
-        # Open files with Dask
-        templates = {c: read_template.format(c) for c in self.channels}
-        openfiles = {c: dask.bytes.open_files(temp) for c,temp in templates.items()}
-        # Get headers from all FITS files
-        self.headers = self._get_headers(openfiles)
-        # Get dtype and shape
-        self.dtype,self.shape = self._get_dtype_and_shape()
-        # Construct maps
-        self.maps = self._get_maps(openfiles)
-        
-    def _get_maps(self,openfiles,):
-        arrays = {c: [da.from_array(DelayedFITS(f,shape=self.shape,dtype=self.dtype,hdu=0),chunks=self.shape) 
-                      for f in files] 
-                  for c,files in openfiles.items()}
-        maps = {}
-        for c in arrays.keys():
-            maps[c] = [delayed(sunpy.map.sources.AIAMap)(a, h) for a,h in zip(arrays[c], self.headers[c])]
-        return maps
-        
-    def _get_headers(self, openfiles):
-        client = distributed.get_client()
-        futures = {c: client.map(get_header, fn) for c,fn in openfiles.items()}
-        return {c: client.gather(fut) for c,fut in futures.items()}
-    
-    def _get_dtype_and_shape(self,):
-        dtypes = {c: [validate_dtype_shape(h) for h in head] for c, head in self.headers.items()}
-        if not all([d == list(dtypes.values())[0] for d in dtypes.values()]):
+        futures = client.map(get_header, openfiles)
+        return client.gather(futures)
+
+    @staticmethod
+    def _get_dtype_and_shape(headers):
+        dtypes = [validate_dtype_shape(h) for h in headers]
+        if not all([d == dtypes[0] for d in dtypes]):
             raise ValueError('All maps must have same shape and dtype')
-        return list(dtypes.values())[0][0]
-    
+        return dtypes[0]
+
     @property
     def time(self,):
         """
-        Note that this should really check for date-obs and then do something different if that exists
+        Note that this should really check for date-obs and then do something different if that
+        exists
         """
-        return u.Quantity([h['t_obs'] for h in self.headers[self.channels[0]]], 
+        return u.Quantity([h['t_obs'] for h in self.headers],
                           self.headers[self.channels[0]][0]['tunit'])
-    
+
     @property
     def unstacked_data(self,):
-        return {c: [da.from_delayed(m.data, dtype=self.dtype, shape=self.shape) for m in maps] for c,maps in self.maps.items()}
-    
+        return [da.from_delayed(m.data, dtype=m.data.dtype, shape=m.data.shape) for m in self.maps]
+
     @property
     def stacked_data(self,):
-        return {c: da.stack(array) for c,array in self.unstacked_data.items()}
-        
-    def rechunk(self,shape):
-        return {c: d.rechunk(shape) for c,d in self.stacked_data.items()}
-    
+        return da.stack(self.unstacked_data)
+
+    def rechunk(self, shape):
+        return self.stacked_data.rechunk(shape)
+
+    def prep(self,):
+        """
+        Lazily apply a prep operation to all maps and return a new distributed cube object
+        """
+        pass
+
+    def derotate(self, reference_date):
+        """
+        Lazily apply a derotation to all maps and return a new distributed cube object
+        """
+        pass
+
+
+class DistributedAIACollection(object):
+    """
+    A collection of distributed AIA images over multiple wavelengths
+
+    #TODO: refactor this to use ndcube sequence
+    #TODO: figure out useful methods to add here
+    #NOTE: It is assumed that the data in this container are all aligned, i.e. prepped and derotated
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Check all spatial and time shapes the same
+        if not all([a.shape == args[0].shape for a in args]):
+            raise ValueError('All spatial dimensions must be the same')
+        if not all([a.time.shape == args[0].time.shape for a in args]):
+            raise ValueError('All time dimensions must be the same')
+        self._cubes = {f"{a.headers[0]['wavelnth']}": a for a in args}
+        self.channels = self._cubes.keys()
+
+    def __getitem__(self, channel):
+        channel = f'{channel}'
+        return self._cubes[channel]
--- scripts/timelags.py
+++ scripts/timelags.py
@@ -8,7 +8,7 @@ import warnings
 import h5py
 import numpy as np
 from scipy.interpolate import interp1d
-import dask.array
+import dask.array as da
 from sunpy.map import Map, GenericMap
 from sunpy.util.metadata import MetaDict
 from astropy.coordinates import SkyCoord
@@ -17,111 +17,63 @@ from astropy.utils.console import ProgressBar
 
 from synthesizAR.util import get_keys
 
+from aiacube import DistributedAIACollection
 
-class AIATimeLags(object):
+
+class AIATimeLags(DistributedAIACollection):
     """
-    Compute AIA timelag maps from many synthesized AIA observations
-
-    Parameters
-    ----------
-    instr : synthesizAR.instruments.InstrumentSDOAIA
-    fits_root_path : str
+    Compute AIA timelag maps
     """
+    @property
+    def timelags(self):
+        c = self.channels[0]
+        delta_t = np.diff(self[c].time.value).cumsum()
+        return np.hstack([-delta_t[::-1], np.array([0]), delta_t]) * self[c].time.unit
 
-    def __init__(self, instr, hdf5_filename, fits_root_path=None, **kwargs):
-        self.instr = instr
-        delta_t = np.diff(instr.observing_time.value).cumsum()
-        self.timelags = (np.hstack([-delta_t[::-1], np.array([0]), delta_t])
-                         * self.instr.observing_time.unit)
-        self.hdf5_filename = hdf5_filename
-        if fits_root_path:
-            ff_format = os.path.join(fits_root_path,
-                                     f'{instr.name}', '{channel}', 'map_t{i_time:06d}.fits')
-            self.load_data(ff_format, **kwargs)
+    def make_timeseries(self, channel, left_corner, right_corner, **kwargs):
+        tmp = self[channel].maps[0].compute()
+        x_l, y_l = tmp.world_to_pixel(SkyCoord(*left_corner, frame=tmp.coordinate_frame))
+        x_u, y_u = tmp.world_to_pixel(SkyCoord(*right_corner, frame=tmp.coordinate_frame))
+        x_l, y_l, x_u, y_u = np.round([x_l.value, y_l.value, x_u.value, y_u.value]).astype(np.int)
+        chunks = kwargs.get('chunks', (tmp.data.shape[0]//10, tmp.data.shape[1]//10))
+        return (self[channel].rechunk(self[channel].time.shape+chunks)[:, y_l:y_u, x_l:x_u]
+                .mean(axis=(1, 2)))
 
-    def get_metadata(self, channel_name):
-        with h5py.File(self.hdf5_filename, 'r') as hf:
-            meta = dict(hf[channel_name]['meta'].attrs)
-        return MetaDict(meta)
-
-    def get_data(self, channel_name):
-        hf = h5py.File(self.hdf5_filename, 'r')
-        return dask.array.from_array(hf[channel_name]['data'], hf[channel_name]['data'].chunks)
-
-    def load_data(self, ff_format, **kwargs):
-        """
-        Create data cubes over desired time range for all channels
-        """
-        with h5py.File(self.instr.counts_file, 'r') as hf:
-            instr_time = u.Quantity(hf['time'], get_keys(hf['time'].attrs, ('unit','units')))
-        with ProgressBar(len(self.instr.channels) * len(self.instr.observing_time),
-                         ipython_widget=kwargs.get('notebook', True)) as progress:
-            with h5py.File(self.hdf5_filename, 'w') as hf:
-                for channel in self.instr.channels:
-                    grp = hf.create_group(channel['name'])
-                    # Read in tmp file to get shape
-                    i_time = np.where(self.instr.observing_time[0] == instr_time)[0][0]
-                    tmp = Map(ff_format.format(channel=channel['name'], i_time=i_time))
-                    shape = tmp.data.shape + self.instr.observing_time.shape
-                    chunks = kwargs.get('chunks', (shape[0]//20, shape[1]//20, shape[2]))
-                    dset = grp.create_dataset('data', shape, chunks=chunks)
-                    tmp_array = np.zeros(shape)
-                    for i, t in enumerate(self.instr.observing_time):
-                        i_time = np.where(t == instr_time)[0][0]
-                        tmp = Map(ff_format.format(channel=channel['name'], i_time=i_time))
-                        tmp_array[:, :, i] = tmp.data
-                        progress.update()
-                    dset[:, :, :] = tmp_array
-                    # Add map metadata as attributes
-                    meta = grp.create_group('meta')
-                    for k in tmp.meta:
-                        try:
-                            meta.attrs[k] = tmp.meta[k]
-                        except TypeError:
-                            continue
-
-    def make_timeseries(self, channel, left_corner, right_corner):
-        darray = self.get_data(channel)
-        tmp = Map(np.array(darray[:, :, 0]), self.get_metadata(channel))
-        blc = tmp.world_to_pixel(SkyCoord(*left_corner, frame=tmp.coordinate_frame))
-        trc = tmp.world_to_pixel(SkyCoord(*right_corner, frame=tmp.coordinate_frame))
-        ts = darray[round(blc.y.value):round(trc.y.value),
-                    round(blc.x.value):round(trc.x.value), :].mean(axis=(0, 1)).compute()
-        return ts
-
-    def correlation_1d(self, channel_a, channel_b, left_corner, right_corner):
-        ts_a = self.make_timeseries(channel_a, left_corner, right_corner)
-        ts_b = self.make_timeseries(channel_b, left_corner, right_corner)
+    def correlation_1d(self, channel_a, channel_b, left_corner, right_corner, **kwargs):
+        ts_a = self.make_timeseries(channel_a, left_corner, right_corner, **kwargs)
+        ts_b = self.make_timeseries(channel_b, left_corner, right_corner, **kwargs)
         ts_a = (ts_a - ts_a.mean()) / ts_a.std()
         ts_b = (ts_b - ts_b.mean()) / ts_b.std()
-        cc = np.fft.irfft(np.fft.rfft(ts_a[::-1], n=self.timelags.shape[0])
-                          * np.fft.rfft(ts_b, n=self.timelags.shape[0]), n=self.timelags.shape[0])
+        cc = da.fft.irfft(da.fft.rfft(ts_a[::-1], n=self.timelags.shape[0])
+                          * da.fft.rfft(ts_b, n=self.timelags.shape[0]), n=self.timelags.shape[0])
         return cc
 
-    def correlation_2d(self, channel_a, channel_b):
+    def correlation_2d(self, channel_a, channel_b, **kwargs):
         """
         Create Dask task graph to compute cross-correlation using FFT for each pixel in an AIA map
         """
-        darray_a = self.get_data(channel_a)[:, :, ::-1]
-        darray_b = self.get_data(channel_b)
+        shape = self[channel_a].maps[0].data.shape
+        chunks = kwargs.get('chunks', (shape[0]//10, shape[1]//10))
+        cube_a = self[channel_a].rechunk(self[channel_a].time.shape+chunks)[::-1, :, :]
+        cube_b = self[channel_b].rechunk(self[channel_b].time.shape+chunks)
         # Normalize
-        std_a = darray_a.std(axis=2)
-        std_a = dask.array.where(std_a == 0, 1, std_a)
-        v_a = (darray_a - darray_a.mean(axis=2)[:, :, np.newaxis]) / std_a[:, :, np.newaxis]
-        std_b = darray_b.std(axis=2)
-        std_b = dask.array.where(std_b == 0, 1, std_b)
-        v_b = (darray_b - darray_b.mean(axis=2)[:, :, np.newaxis]) / std_b[:, :, np.newaxis]
+        std_a = cube_a.std(axis=0)
+        std_a = da.where(std_a == 0, 1, std_a)
+        v_a = (cube_a - cube_a.mean(axis=0)[np.newaxis, :, :]) / std_a[np.newaxis, :, :]
+        std_b = cube_b.std(axis=0)
+        std_b = da.where(std_b == 0, 1, std_b)
+        v_b = (cube_b - cube_b.mean(axis=0)[np.newaxis, :, :]) / std_b[np.newaxis, :, :]
         # Fast Fourier Transform of both channels
-        fft_a = dask.array.fft.rfft(v_a, axis=2, n=self.timelags.shape[0])
-        fft_b = dask.array.fft.rfft(v_b, axis=2, n=self.timelags.shape[0])
+        fft_a = da.fft.rfft(v_a, axis=0, n=self.timelags.shape[0])
+        fft_b = da.fft.rfft(v_b, axis=0, n=self.timelags.shape[0])
         # Inverse of product of FFTS to get cross-correlation (by convolution theorem)
-        return dask.array.fft.irfft(fft_a * fft_b, axis=2, n=self.timelags.shape[0])
+        return da.fft.irfft(fft_a * fft_b, axis=0, n=self.timelags.shape[0])
 
     def make_correlation_map(self, channel_a, channel_b, **kwargs):
         """
         Build map of max correlation value between two AIA channels
         """
-        cc = self.correlation_2d(channel_a, channel_b)
+        cc = self.correlation_2d(channel_a, channel_b, **kwargs)
         bounds = kwargs.get('timelag_bounds', None)
         if bounds is not None:
             indices, = np.where(np.logical_and(self.timelags >= bounds[0],
@@ -131,9 +83,9 @@ class AIATimeLags(object):
         else:
             start = 0
             stop = self.timelags.shape[0] + 1
-        max_cc = cc[:,:,start:stop].max(axis=2).compute()
+        max_cc = cc[start:stop, :, :].max(axis=0).compute()
         # Metadata
-        meta = self.get_metadata(channel_a).copy()
+        meta = self[channel_a].headers[0].copy()
         del meta['instrume']
         del meta['t_obs']
         del meta['wavelnth']
@@ -150,8 +102,10 @@ class AIATimeLags(object):
         """
         Compute map of timelag associated with maximum cross-correlation between
         two channels in each pixel of an AIA map.
+
+        #NOTE: Extra block for computing correlation map is to save time
         """
-        cc = self.correlation_2d(channel_a, channel_b)
+        cc = self.correlation_2d(channel_a, channel_b, **kwargs)
         bounds = kwargs.get('timelag_bounds', None)
         if bounds is not None:
             indices, = np.where(np.logical_and(self.timelags >= bounds[0],
@@ -163,14 +117,14 @@ class AIATimeLags(object):
             stop = self.timelags.shape[0] + 1
         if kwargs.get('return_correlation_map'):
             _cc = cc.compute()
-            max_cc = _cc[:,:,start:stop].max(axis=2)
-            i_max_cc = _cc[:,:,start:stop].argmax(axis=2)
+            max_cc = _cc[start:stop, :, :].max(axis=0)
+            i_max_cc = _cc[start:stop, :, :].argmax(axis=0)
             del _cc
         else:
-            i_max_cc = cc[:,:,start:stop].argmax(axis=2).compute()
+            i_max_cc = cc[start:stop, :, :].argmax(axis=0).compute()
         max_timelag = self.timelags[start:stop][i_max_cc]
         # Metadata
-        meta = self.get_metadata(channel_a).copy()
+        meta = self[channel_a].headers[0].copy()
         del meta['instrume']
         del meta['t_obs']
         del meta['wavelnth']
@@ -187,80 +141,6 @@ class AIATimeLags(object):
             del plot_settings['vmin']
             del plot_settings['vmax']
             correlation_map = GenericMap(max_cc, meta, plot_settings=plot_settings)
-            return timelag_map,correlation_map
+            return timelag_map, correlation_map
         else:
             return timelag_map
-    
-    
-class AIATimeLagsObserved(AIATimeLags):
-    
-    def __init__(self, hdf5_filename, fits_root_path=None, **kwargs):
-        self.hdf5_filename = hdf5_filename
-        if fits_root_path is not None:
-            self.load_data(fits_root_path, **kwargs)
-    
-    def load_data(self, fits_root_path, **kwargs):
-        """
-        Save all AIA maps to HDF5 file. Note that these maps must already be prepped,
-        derotated, and cropped.
-        """
-        channels = [94, 131, 171, 193, 211, 335]
-        cadence = 12.0
-        # Find time interval and reference time for each channel
-        ref_time = {}
-        time_min, time_max = None, None
-        for chan in channels:
-            tmp_list = sorted(glob.glob(os.path.join(fits_root_path, f'*_{chan}_*.fits')))
-            tmp = Map(tmp_list[0], tmp_list[-1])
-            if time_min is None or tmp[0].date < time_min:
-                time_min = tmp[0].date
-            if time_max is None or tmp[1].date > time_max:
-                time_max = tmp[1].date
-            ref_time[f'{chan}'] = tmp[0].date
-            shape = tmp[0].data.shape
-        # Save common time
-        common_time = np.arange(0., (time_max - time_min).seconds, cadence) * u.s
-        with h5py.File(self.hdf5_filename, 'w') as hf:
-            dset = hf.create_dataset('observing_time', data=common_time.value)
-            dset.attrs['unit'] = common_time.unit.to_string()
-        chunks = kwargs.get('chunks', (shape[0]//20, shape[1]//20, common_time.shape[0]))
-        # Load all maps from all channels
-        with ProgressBar(len(glob.glob(os.path.join(fits_root_path, '*'))), ipython_widget=kwargs.get('notebook', True)) as progress:
-            for chan in channels:
-                files = sorted(glob.glob(os.path.join(fits_root_path, f'*_{chan}_*.fits')))
-                # Load all data into memory
-                tmp_time = np.zeros((len(files),))
-                tmp_array = np.empty(shape + (len(files),))
-                for i,f in enumerate(files):
-                    # load file
-                    tmp_map = Map(f)
-                    tmp_array[:, :, i] = tmp_map.data
-                    # get time
-                    tmp_time[i] = (tmp_map.date - ref_time[f'{chan}']).seconds
-                    progress.update()
-                # interpolate to common time
-                tmp_array = interp1d(tmp_time, tmp_array, axis=2, fill_value='extrapolate', 
-                                     kind='linear', bounds_error=False, assume_sorted=True)(common_time.value)
-                # Load all data into an HDF5 file
-                with h5py.File(self.hdf5_filename, 'a') as hf:
-                    grp = hf.create_group(f'{chan}')
-                    dset = grp.create_dataset('data', shape+common_time.shape, chunks=chunks)
-                    dset[:,:,:] = tmp_array
-                    meta = grp.create_group('meta')
-                    for k in tmp_map.meta:
-                        try:
-                            meta.attrs[k] = tmp_map.meta[k]
-                        except TypeError:
-                            continue
-                    
-    @property
-    def timelags(self):
-        delta_t = np.diff(self.observing_time.value).cumsum()
-        return (np.hstack([-delta_t[::-1], np.array([0]), delta_t]) 
-                * self.observing_time.unit)
-    
-    @property
-    def observing_time(self):
-        with h5py.File(self.hdf5_filename,'r') as hf:
-            obs_time = u.Quantity(hf['observing_time'], get_keys(hf['observing_time'].attrs, ('unit', 'units')))
-        return obs_time
