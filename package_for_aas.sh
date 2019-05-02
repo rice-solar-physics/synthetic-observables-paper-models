8a3b0d929a44169a0845db10322f42e0959aa8a4
Will Barnes <wtb2@rice.edu>
2019-05-02 16:28:16 -0500

    add option for generating cover letter

--- package_for_aas.sh
+++ package_for_aas.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# Package up paper files for submission to ApJ, arXiv, etc.
+# Package up paper files for submission to ApJ, arXiv, etc. This 
+# compiles all the TeX source files into a single file, removes the
+# dependency on PythonTeX, generates all of the figures, and fixes
+# the figure paths.
+#
 # Usage:
 #   ./package_for_aas.sh /path/to/top/level/dir
+#   ./package_for_aas.sh /path/to/top/level/dir --cover-letter
 
 # Create target directory
 DIR_NAME="aas-submission"
 DEST_DIR="$1/$DIR_NAME"
 mkdir -p $DEST_DIR
+
+# Check for cover letter option
+if [[ $* == *--cover-letter* ]]
+then
+    COVER_LETTER=true
+else
+    COVER_LETTER=false
+fi
 
 # Copy over all paper files
 cp -r paper/sections $DEST_DIR
@@ -20,6 +33,7 @@ cp paper/paper.tex $DEST_DIR
 cp paper/references.bib $DEST_DIR
 cp paper/software.bib $DEST_DIR
 cp fix_figure_paths.py $DEST_DIR
+if $COVER_LETTER ; then cp cover-letter.md $DEST_DIR ; fi
 
 # Flatten to a single file
 cd $DEST_DIR
@@ -46,6 +60,12 @@ bibtex paper
 pdflatex -synctex=1 -interaction=nonstopmode -file-line-error paper.tex
 pdflatex -synctex=1 -interaction=nonstopmode -file-line-error paper.tex
 
+# (optionally) Generate PDF of cover letter
+if $COVER_LETTER
+then
+    pandoc -s cover-letter.md -o cover-letter.pdf --bibliography="references.bib"
+fi
+
 # Cleanup
 rm -r sections
 rm -r data
@@ -61,6 +81,7 @@ rm *.pytxcode
 rm *.synctex.gz
 rm *.py
 rm pythontex.tex
+if $COVER_LETTER ; then rm cover-letter.md ; fi
 
 # Compress directory and cleanup
 cd $1
